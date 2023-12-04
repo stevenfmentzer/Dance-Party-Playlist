50522cf0a788878b09f8eade03b23e6e3a886d69
Carlos <bowyercarlos@gmail.com>
2023-12-04 18:51:24 -0500

    rating tool added

--- src/index.js
+++ src/index.js
@@ -7,10 +7,20 @@ fetch('http://localhost:3000/playlists')
 
 
 
-const ratingSlider = document.getElementById('ratingSlider');
-  const ratingValue = document.getElementById('ratingValue');
+// const ratingSlider = document.getElementById('ratingSlider');
+//   const ratingValue = document.getElementById('ratingValue');
 
-  ratingSlider.addEventListener('input', function() {
-    const value = (parseFloat(this.value) / 10).toFixed(1); // Convert slider value to 0-1 range
-    ratingValue.textContent = value;
-  });
+//   ratingSlider.addEventListener('input', function() {
+//     const value = (parseFloat(this.value) / 10).toFixed(1); // Convert slider value to 0-1 range
+//     ratingValue.textContent = value;
+//   });
+
+function submitRating() {
+    const selectedRating = document.querySelector('input[name="rating"]:checked');
+    if (selectedRating) {
+      alert('You rated: ' + selectedRating.value);
+      // You can handle the rating submission here, e.g., send it to a server
+    } else {
+      alert('Please select a rating.');
+    }
+  }
