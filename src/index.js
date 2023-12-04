7126cf38d17805ae60a2a77bafe0631be40daebe
Carlos <bowyercarlos@gmail.com>
2023-12-04 17:16:02 -0500

    rating bar added

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@
 fetch('http://localhost:3000/playlists')
 .then(response => response.json())
 .then(console.log)
+
+
+
+
+const ratingSlider = document.getElementById('ratingSlider');
+  const ratingValue = document.getElementById('ratingValue');
+
+  ratingSlider.addEventListener('input', function() {
+    const value = (parseFloat(this.value) / 10).toFixed(1); // Convert slider value to 0-1 range
+    ratingValue.textContent = value;
+  });
