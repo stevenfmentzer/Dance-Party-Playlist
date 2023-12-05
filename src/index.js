8998b114cafc715ebef1dfb5c0370b677266ab2f
melissavelasquezgreene <melissa0191@outlook.es>
2023-12-05 11:21:59 -0500

    changes made by melissa

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 
 
+
 fetch('http://localhost:3000/playlists')
-.then(response => response.json())
-.then(console.log)
+  .then(response => response.json())
+  .then(albums => {
+    renderPlaylists(albums);
+  });
+
+function renderPlaylists(albums) {
+  playlists.forEach(renderSinglePlaylist);
+}
+
+function renderSinglePlaylist(albulms) {
+  const newElement = document.getElementById("album-art");
+  const spanElement = document.createElement("img");
+  spanElement.src = album.tracks;
+  newElement.append(spanElement);
+}
+
